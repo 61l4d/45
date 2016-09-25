1335e09d64d6b3d0dcaccf17d6e88a95fabc5ecf
uu <giladbarkan@live.com>
2016-09-24 21:32:32 -0400

    completed oauth backend login flow for se

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -38,6 +38,41 @@ class CallbacksController < ApplicationController
   end
 
   def se
-    raise params.inspect
+    # state mismatch
+    if session[:state].nil? or params[:state] != session[:state]
+      raise "State mismatch. Session state: " + session[:state] 
+          + " Params: " + params.inspect
+    end
+
+    # error, such as user denies permission
+    raise params.inspect if not params[:error].nil? or not params[:error_id].nil?
+  
+    # code seems to be missing
+    if params[:code].nil?
+      raise "Code seems to be missing. Params: " + params.inspect
+
+    # get access token
+    else
+      redirect_uri = ENV['BASE_URL'] + "callbacks/se"
+
+      response = Unirest.post "https://stackexchange.com/oauth/access_token",
+                 parameters: {
+                   client_id: ENV['SE_CLIENT_ID'],
+                   redirect_uri: redirect_uri,
+                   client_secret: ENV['SE_CLIENT_SECRET'],
+                   code: params[:code]
+                 }
+
+      body = response.body
+      
+      # access token missing
+      raise "Stack Exchange access token seems to be missing. Response: " + response.inspect if not body['access_token']
+
+      session[:se] = body
+     
+raise session[:se].inspect
+      # redirect to index page
+      redirect_to 'home/index'
+    end
   end
 end
