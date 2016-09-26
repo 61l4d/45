a4372b76bb9d8261e30a067d8a3ecb76020c821f
uu <giladbarkan@live.com>
2016-09-26 15:46:35 -0400

    added CGI::parse for session[:se]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,15 @@ class SessionsController < ApplicationController
 
   def create_account
     # retrieve fb and se user_id's
-    raise [session[:fb],session[:se]].inspect
+=begin
+    response = Unirest.get "https://graph.facebook.com/v2.7/oauth/access_token",
+               parameters: {
+                 access_token: session[:fb]
+               }
+
+    body = response.body
+=end
+    raise [session[:fb],CGI::parse(session[:se])].inspect
   end
 
   def destroy
